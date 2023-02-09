20c3f116acd8cb74366dff8c4a7cd25c6ba9319c
Alexander Bell <>
2023-02-08 19:58:12 -0800

    Add line breaks

--- content-aside.php
+++ content-aside.php
@@ -8,7 +8,9 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
-        <h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'firstresponse' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+        <h2 class="entry-title">
+            <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'firstresponse' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+        </h2>
     </header><!-- .entry-header -->
 
     <?php if ( is_search() ) : // Only display Excerpts for Search ?>
