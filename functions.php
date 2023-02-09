44b6704923930796ba9f3355da0f24994d8f5228
Alexander Bell <>
2023-02-08 20:00:06 -0800

    Fix time ago function

--- functions.php
+++ functions.php
@@ -206,116 +206,123 @@
      
     endif;
      
-    function firstresponse_time_ago()
-    {
-    global $post;
-    $date = get_post_time('G', true, $post);
-    /**
-     * Where you see 'firstresponse' below, you'd
-     * want to replace those with whatever term
-     * you're using in your theme to provide
-     * support for localization.
-     */
-     
-    // Array of time period chunks
-     
-    $chunks = array(
-    array(
-    60 * 60 * 24 * 365,
-    __('year', 'firstresponse') ,
-    __('years', 'firstresponse')
-    ) ,
-    array(
-    60 * 60 * 24 * 30,
-    __('month', 'firstresponse') ,
-    __('months', 'firstresponse')
-    ) ,
-    array(
-    60 * 60 * 24 * 7,
-    __('week', 'firstresponse') ,
-    __('weeks', 'firstresponse')
-    ) ,
-    array(
-    60 * 60 * 24,
-    __('day', 'firstresponse') ,
-    __('days', 'firstresponse')
-    ) ,
-    array(
-    60 * 60,
-    __('hour', 'firstresponse') ,
-    __('hours', 'firstresponse')
-    ) ,
-    array(
-    60,
-    __('minute', 'firstresponse') ,
-    __('minutes', 'firstresponse')
-    ) ,
-    array(
-    1,
-    __('second', 'firstresponse') ,
-    __('seconds', 'firstresponse')
-    )
-    );
-    if (!is_numeric($date)) {
-    $time_chunks = explode(':', str_replace(' ', ':', $date));
-    $date_chunks = explode('-', str_replace(' ', '-', $date));
-    $date = gmmktime((int)$time_chunks[1], (int)$time_chunks[2], (int)$time_chunks[3], (int)$date_chunks[1], (int)$date_chunks[2], (int)$date_chunks[0]);
-    }
-     
-    $current_time = current_time('mysql', $gmt = 0);
-    $newer_date = time();
-     
-    // Difference in seconds
-     
-    $since = $newer_date - $date;
-     
-    // Something went wrong with date calculation and we ended up with a negative date.
-     
-    if (0 > $since) return __('sometime', 'firstresponse');
-    /**
-     * We only want to output one chunks of time here, eg:
-     * x years
-     * xx months
-     * so there's only one bit of calculation below:
-     */
-     
-    // Step one: the first chunk
-     
-    for ($i = 0, $j = count($chunks); $i < $j; $i++) {
-    $seconds = $chunks[$i][0];
-     
-    // Finding the biggest chunk (if the chunk fits, break)
-     
-    if (($count = floor($since / $seconds)) != 0) break;
-    }
-     
-    // Set output var
-     
-    $output = (1 == $count) ? '1 ' . $chunks[$i][1] : $count . ' ' . $chunks[$i][2];
-    if (!(int)trim($output)) {
-    $output = '0 ' . __('seconds', 'firstresponse');
-    }
-     
-    $output.= __(' ago', 'firstresponse');
-    return $output;
+    function firstresponse_time_ago() {
+        global $post;
+        $date = get_post_time('G', true, $post);
+        /**
+         * Where you see 'firstresponse' below, you'd
+         * want to replace those with whatever term
+         * you're using in your theme to provide
+         * support for localization.
+         */
+        
+        // Array of time period chunks
+        
+        $chunks = array(
+            array(
+                60 * 60 * 24 * 365,
+                __('year', 'firstresponse') ,
+                __('years', 'firstresponse')
+            ),
+            array(
+                60 * 60 * 24 * 30,
+                __('month', 'firstresponse'),
+                __('months', 'firstresponse')
+            ),
+            array(
+                60 * 60 * 24 * 7,
+                __('week', 'firstresponse'),
+                __('weeks', 'firstresponse')
+            ),
+            array(
+                60 * 60 * 24,
+                __('day', 'firstresponse'),
+                __('days', 'firstresponse')
+            ),
+            array(
+                60 * 60,
+                __('hour', 'firstresponse'),
+                __('hours', 'firstresponse')
+            ),
+            array(
+                60,
+                __('minute', 'firstresponse'),
+                __('minutes', 'firstresponse')
+            ),
+            array(
+                1,
+                __('second', 'firstresponse'),
+                __('seconds', 'firstresponse')
+            )
+        );
+        if (!is_numeric($date)) {
+            $time_chunks = explode(':', str_replace(' ', ':', $date));
+            $date_chunks = explode('-', str_replace(' ', '-', $date));
+            if ($time_chunks[1] ?? null) {
+                $date = gmmktime(
+                    (int)$time_chunks[1],
+                    (int)$time_chunks[2],
+                    (int)$time_chunks[3],
+                    (int)$date_chunks[1],
+                    (int)$date_chunks[2],
+                    (int)$date_chunks[0]
+                );
+            }
+        }
+        
+        $current_time = current_time('mysql', $gmt = 0);
+        $newer_date = time();
+        
+        // Difference in seconds
+        
+        $since = $newer_date - $date;
+        
+        // Something went wrong with date calculation and we ended up with a negative date.
+        
+        if (0 > $since) return __('sometime', 'firstresponse');
+        /**
+         * We only want to output one chunks of time here, eg:
+         * x years
+         * xx months
+         * so there's only one bit of calculation below:
+         */
+        
+        // Step one: the first chunk
+        
+        for ($i = 0, $j = count($chunks); $i < $j; $i++) {
+            $seconds = $chunks[$i][0];
+            
+            // Finding the biggest chunk (if the chunk fits, break)
+            
+            if (($count = floor($since / $seconds)) != 0) break;
+        }
+        
+        // Set output var
+        
+        $output = (1 == $count) ? '1 ' . $chunks[$i][1] : $count . ' ' . $chunks[$i][2];
+        if (!(int)trim($output)) {
+            $output = '0 ' . __('seconds', 'firstresponse');
+        }
+        
+        $output.= __(' ago', 'firstresponse');
+        return $output;
     }
      
     // Filter our firstresponse_time_ago() function into WP's the_time() functionfunction wp_change_target($content){
      
-    return preg_replace_callback('/<a[^>]+/', 'wp_target_callback', $content);
+    return preg_replace_callback('/<a[^>]+/', 'wp_target_callback', firstresponse_time_ago());
      
-    function wp_target_callback($matches)
-    {
-    $link = $matches[0];
-    $mu_url = get_bloginfo('url');
-    if (strpos($link, 'target') === false) {
-    $link = preg_replace("%(href=\S(?!$mu_url))%i", 'target="_blank" $1', $link);
-    }
-    elseif (preg_match("%href=\S(?!$mu_url)%i", $link)) {
-    $link = preg_replace('/target=S(?!_blank)\S*/i', 'target="_blank"', $link);
-    }
-     
-    return $link;
+    function wp_target_callback($matches) {
+        $link = $matches[0];
+        $mu_url = get_bloginfo('url');
+        if (strpos($link, 'target') === false) {
+            $link = preg_replace("%(href=\S(?!$mu_url))%i", 'target="_blank" $1', $link);
+        }
+        elseif (preg_match("%href=\S(?!$mu_url)%i", $link)) {
+            $link = preg_replace('/target=S(?!_blank)\S*/i', 'target="_blank"', $link);
+        }
+        
+        return $link;
     }
      
     add_filter('the_permalink', 'wp_change_target');
