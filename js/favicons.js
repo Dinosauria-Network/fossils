076491376276556ccabe451239863be64b3bc59f
Alexander Bell <>
2023-02-08 20:01:18 -0800

    Add jQuery wrapper

--- js/favicons.js
+++ js/favicons.js
@@ -1,6 +1,8 @@
-$("a[href^='http'].url").each(function() {
-    $(this).css({
-        background: "url(http://g.etfv.co/' + this.href + ') left center no-repeat",
-        "padding-left": "20px"
-    });
-});
+(function($) {
+    $("a[href^='http'].url").each(function() {
+        $(this).css({
+            background: "url(https://getfavicon.net/' + this.href + ') left center no-repeat",
+            "padding-left": "20px"
+        });
+    });
+}(jQuery));
